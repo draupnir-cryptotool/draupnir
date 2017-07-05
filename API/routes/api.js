d3876fe8c135f32da28796a5e4f394800b3d4b2d
Chris Robertson <chrisxrobertson@gmail.com>
2017-07-06 07:49:29 +1000

    configure eslint and minor refactoring.

--- API/routes/api.js
+++ API/routes/api.js
@@ -8,14 +8,13 @@ const _ = require('lodash');
 
 // Temporary route for testing
 router.get('/btcusd/:amount', function(req, res, next) {
-
-  // Create a fetch promise for each API call. These will all be called by a Promise.all later
+  // Create a fetch promise for each API call.
+  // These will all be called by a Promise.all later
   const fetchBitfinex = fetch('https://api.bitfinex.com/v1/book/btcusd/?limit_bids=0')
     // The APIs return JSON, so we parse it into a JavaScript object
     .then((res) => res.json())
     // When the parsed JSON is ready we can muck around with it
     .then((json) => {
-
       // Just take the part of the returned order book that we want
       orderBook = json.asks;
       // Setup an array to store the orders from the exchange
@@ -33,11 +32,10 @@ router.get('/btcusd/:amount', function(req, res, next) {
       });
       return massagedOrderBook;
     });
-  
+
   const fetchBitstamp = fetch('https://www.bitstamp.net/api/order_book/')
     .then((res) => res.json())
     .then((json) => {
-
       orderBook = json.asks;
       massagedOrderBook = [];
 
@@ -56,7 +54,6 @@ router.get('/btcusd/:amount', function(req, res, next) {
   const fetchBTCe = fetch('https://btc-e.com/api/3/depth/btc_usd/?limit=100')
     .then((res) => res.json())
     .then((json) => {
-
       orderBook = json.btc_usd.asks;
       massagedOrderBook = [];
 
@@ -74,10 +71,12 @@ router.get('/btcusd/:amount', function(req, res, next) {
 
   // Wait for all the API calls to return before we play with the data
   Promise.all([fetchBTCe, fetchBitfinex, fetchBitstamp])
-    .then(orderBooks => {
-      // Promise.all gives us an array of the returned values, so we flatten them into a single array
+    .then((orderBooks) => {
+      // Promise.all gives us an array of the returned values,
+      // so we flatten them into a single array
       fullOrderBook = _.flattenDeep(orderBooks);
-      // We now have a single array of objects, so we sort them by their price property
+      // We now have a single array of objects,
+      // so we sort them by their price property
       fullOrderBook = _.sortBy(fullOrderBook, ['price']);
 
       orderAmount = req.params.amount;
@@ -87,13 +86,12 @@ router.get('/btcusd/:amount', function(req, res, next) {
       for (let order of fullOrderBook) {
         if (orderTally >= orderAmount) {
           break;
-        }
-        else if(order.orderTotal < (orderAmount - orderTally)) {
+        } else if (order.orderTotal < (orderAmount - orderTally)) {
           orderTally += order.orderTotal;
           fulfilledOrderBook.push(order);
-        }
-        // Order is more than what we need to fulfil requested amount, we only need part of it
-        else {
+          // Order is more than what we need to fulfil requested amount,
+          // we only need part of it
+        } else {
           partialOrder = order;
           partialOrder.amount = (orderAmount - orderTally) / partialOrder.price;
           partialOrder.orderTotal = partialOrder.price * partialOrder.amount;
