5c67d20c2e58a5b01df1a04c91e780dc9f7981b6
Jaime Terreu <jaime@terreu.com>
2017-07-05 19:11:28 +1000

    added order schema and saving to db

--- API/routes/clientRouter.js
+++ API/routes/clientRouter.js
@@ -2,15 +2,16 @@ const express = require('express')
 const Client = require('../models/Clients')
 const router = express.Router()
 
+// create new client
 router.post('/client/new', (req, res) => {
   const newclient = req.body
-    Client.create(newclient)
-    .then((client) => {
-      res.json(client)
-    })
-    .catch((err) => {
-      res.json({ err: 'You fucked up' })
-    })
+  Client.create(newclient)
+  .then((client) => {
+    res.json(client)
+  })
+  .catch((err) => {
+    res.json({ err: 'You fucked up' })
+  })
 })
 
 module.exports = router
