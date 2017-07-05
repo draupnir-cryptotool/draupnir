3fc54c7f297603697f3a1f4d7c43678a437e7edc
Jaime Terreu <jaime@terreu.com>
2017-07-05 17:10:36 +1000

    client route and POST operational

---
+++ API/routes/clientRouter.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const Client = require('../models/Clients')
+const router = express.Router()
+
+router.post('/client/new', (req, res) => {
+  const newclient = req.body
+    Client.create(newclient)
+    .then((client) => {
+      res.json(client)
+    })
+    .catch((err) => {
+      res.json({ err: 'You fucked up' })
+    })
+})
+
+module.exports = router
