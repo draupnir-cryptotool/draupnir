d9831fa72cb75fc8349c7529bfa54019e1e5103c
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-07-07 18:53:59 +1000

    API: initiate user authentication set up

---
+++ API/middleware/auth.js
@@ -0,0 +1,26 @@
+const passport = require('passort')
+const User = require('../models/user')
+
+// Add local strategy
+passport.use(User.createStrategy())
+
+// Register new user
+function registerMiddleware(req, res, next) {
+  const user = new User({
+    email: req.body.email
+  })
+  User.register(user, req.body.password, (error, user) => {
+    if (error) {
+      next(error)
+      return
+    }
+    req.user = user
+    next()
+  })
+}
+
+module.exports = {
+  initialize: passport.initialize(),
+  authenticateSignIn: passport.authenticate('local', {session: false}),
+  register: registerMiddleware
+}
