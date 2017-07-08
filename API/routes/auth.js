cb2f55f0a1a93f9caad1c0c6158ab049988da646
Vignesh Natarajan <vigneshnataraj@gmail.com>
2017-07-08 17:33:11 +1000

    API: change registrtion route

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -3,7 +3,7 @@ const authMiddleware = require('../middleware/auth')
 const router = express.Router()
 
 // Registration: POST /auth/register
-router.post('/auth', authMiddleware.register, authMiddleware.signtokenHandler)
+router.post('/register', authMiddleware.register, authMiddleware.signtokenHandler)
 
 // Sign in: POST /auth
 router.post('/signin', authMiddleware.authenticateSignIn, authMiddleware.signtokenHandler)
