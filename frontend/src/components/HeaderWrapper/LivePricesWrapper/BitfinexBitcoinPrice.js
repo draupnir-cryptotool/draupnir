d89ac5456a5f7b485277f773af8c8c510154792d
Vi Gi <vigneshnataraj@gmail.com>
2017-07-11 23:33:23 +1000

    REACT: refactor and rename component names

---
+++ frontend/src/components/HeaderWrapper/LivePricesWrapper/BitfinexBitcoinPrice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BitfinexBitcoinPrice({
+  bitfinexBtcValue
+}) {
+  return (
+    <div>
+      <h4>{ bitfinexBtcValue }</h4>
+    </div>
+  )
+}
