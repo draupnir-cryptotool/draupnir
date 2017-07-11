aa0d28db7d8ac838c0b2e43365c70ccae2d5db15
Vi Gi <vigneshnataraj@gmail.com>
2017-07-11 22:44:20 +1000

    REACT: minor refactoring to render bitcoin usd rates

--- frontend/src/components/HeaderWrapper/WalletWrapper/Bitcoin.js
+++ frontend/src/components/HeaderWrapper/WalletWrapper/Bitcoin.js
@@ -5,7 +5,7 @@ export default function Bitcoin({
 }) {
   return (
     <div className="bitBalance">
-      <p style={{fontSize: '3.5rem', marginBottom: '0px'}}>BTC-E - </p>
+      <p style={{fontSize: '3.5rem', marginBottom: '0px'}}>BTC - </p>
       <span style={{fontSize: '2.5rem', verticalAlign: 'middle'}}> { bitBalance.final_balance } </span>
     </div>
   )
