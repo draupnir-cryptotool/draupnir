296b8a63449c08a6b9e52e7dc6d845a149638501
Chris Robertson <chrisxrobertson@gmail.com>
2017-07-14 11:34:15 +1000

    Now gets ETH prices from Australian exchanges and calculates the average.

--- API/routes/ausPrices.js
+++ API/routes/ausPrices.js
@@ -6,16 +6,24 @@ const fetch = require('node-fetch');
 
 router.get('/ausPrices', function(req, res, next) {
   let ausPrices = {
-    acxBestSell: 0,
-    btcmBestSell: 0,
-    irBestSell: 0,
-    average: 0,
+    BTC: {
+      acxBestBTC: 0,
+      btcmBestBTC: 0,
+      irBestBTC: 0,
+    },
+    ETH: {
+      acxBestETH: 0,
+      btcmBestETH: 0,
+      irBestETH: 0,
+    },
+    averageBTC: 0,
+    averageETH: 0,
   };
 
   const acxFetch = fetch('https://acx.io:443//api/v2/tickers.json')
     .then((res) => res.json())
     .then((json) => {
-      ausPrices.acxBestSell = parseFloat(json.btcaud.ticker.sell);
+      ausPrices.BTC.acxBestBTC = parseFloat(json.btcaud.ticker.sell);
       return;
     })
     .catch((err) => console.log(err));
@@ -23,17 +31,29 @@ router.get('/ausPrices', function(req, res, next) {
   const btcmFetch = fetch('https://api.btcmarkets.net/market/BTC/AUD/tick')
     .then((res) => res.json())
     .then((json) => {
-      ausPrices.btcmBestSell = json.bestAsk;
-      return;
+      ausPrices.BTC.btcmBestBTC = json.bestAsk;
     })
+    .then(fetch('https://api.btcmarkets.net/market/ETH/AUD/tick')
+      .then((res) => res.json())
+      .then((json) => {
+        ausPrices.ETH.btcmBestETH = json.bestAsk;
+        return;
+      }))
     .catch((err) => console.log(err));
 
   const irFetch = fetch('https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=xbt&secondaryCurrencyCode=aud')
     .then((res) => res.json())
     .then((json) => {
-      // ausPrices.irBestSell = json.CurrentLowestOfferPrice;
+      // ausPrices.BTC.irBestBTC = json.CurrentLowestOfferPrice;
       return;
     })
+    .then(fetch('https://api.independentreserve.com/Public/GetMarketSummary?primaryCurrencyCode=eth&secondaryCurrencyCode=aud')
+      .then((res) => res.json())
+      .then((json) => {
+        ausPrices.ETH.irBestETH = json.CurrentLowestOfferPrice;
+        console.log(ausPrices);
+        return;
+      }))
     .catch((err) => console.log(err));
 
   exchangeAPIs = [
@@ -44,20 +64,29 @@ router.get('/ausPrices', function(req, res, next) {
 
   Promise.all(exchangeAPIs)
     .then((values) => {
-      priceSum = Object.values(ausPrices).reduce((total, current) => {
+      priceSumBTC = Object.values(ausPrices.BTC).reduce((total, current) => {
         return total + current;
-      });
+      }, 0);
+      priceSumETH = Object.values(ausPrices.ETH).reduce((total, current) => {
+        return total + current;
+      }, 0);
       // This will show us how many exchanges return a non-zero price. This
       // lets us disable exchanges that we don't want to include in the average
       // calculation by simply commenting out their code that writes their value
       // to the ausPrices object.
-      let priceValues = Object.values(ausPrices);
-      let nonZeroExchanges = priceValues.reduce((nonZeroCount, price) => {
+      let priceValuesBTC = Object.values(ausPrices.BTC);
+      let nonZeroExchangesBTC = priceValuesBTC.reduce((nonZeroCount, price) => {
+        price > 0 ? nonZeroCount += 1 : 0;
+        return nonZeroCount;
+      }, 0);
+      let priceValuesETH = Object.values(ausPrices.ETH);
+      let nonZeroExchangesETH = priceValuesETH.reduce((nonZeroCount, price) => {
         price > 0 ? nonZeroCount += 1 : 0;
         return nonZeroCount;
       }, 0);
 
-      ausPrices.average = (priceSum / nonZeroExchanges).toFixed(2);
+      ausPrices.averageBTC = (priceSumBTC / nonZeroExchangesBTC).toFixed(2);
+      ausPrices.averageETH = (priceSumETH / nonZeroExchangesETH).toFixed(2);
       res.send(ausPrices);
     })
     .catch((err) => console.log(err.message));
