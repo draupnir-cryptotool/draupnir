c455c7560bf6cbd6e0af2f3c0e25351080f44bb1
Jaime Terreu <jaime@terreu.com>
2017-07-16 19:08:11 +1000

    adding ID to client info

--- frontend/src/components/MainNavWrapper/staging/pages/InfoPage.js
+++ frontend/src/components/MainNavWrapper/staging/pages/InfoPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default function InfoPage({
   client
 }){
   return (
-    <div>
-      <h3>Name: <span>{client.firstname + " " + client.lastname}</span></h3>
-      <h3>Phone number: <span>{client.phone}</span></h3>
-      <h3>Email: <span>{client.email}</span></h3>
+    <div style={{ display: 'flex' }}>
+      <div style={{  marginRight: '20%' }}>
+        <h1>Contact</h1>
+        <h3>Name: <span>{client.firstname + " " + client.lastname}</span></h3>
+        <h3>Phone number: <span>{client.phone}</span></h3>
+        <h3>Email: <span>{client.email}</span></h3>
+      </div>
+      <div>
+        <h1>ID</h1>
+        <Button bsStyle={'primary'}>Add</Button>
+      </div>
     </div>
   )
 }
