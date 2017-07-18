667cf1370a81d8ec39e3101054761ef7d95ee992
Vi Gi <vigneshnataraj@gmail.com>
2017-07-18 15:07:33 +1000

    API: Send mailgin api to key env variable

--- API/routes/mailRouter.js
+++ API/routes/mailRouter.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/mail', (req,res) => {
-  var api_key = 'key-7a457251a10823bdef5a0b4bae356169';
+  var api_key = process.env.MAILGUN_API
   var domain = 'sandboxf47c6cc2f65a472b97244e383b08d3b8.mailgun.org';
   var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
   
