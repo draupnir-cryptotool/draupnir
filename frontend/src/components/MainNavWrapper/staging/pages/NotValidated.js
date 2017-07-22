6f49de72ffb19faae91859dd20fa03d2b02054c0
Vi Gi <vigneshnataraj@gmail.com>
2017-07-22 22:09:30 +1000

    REACT: complete status page

--- frontend/src/components/MainNavWrapper/staging/pages/NotValidated.js
+++ frontend/src/components/MainNavWrapper/staging/pages/NotValidated.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function NotValidated() {
   return (
-    <div style={{ width: "135", height: "30px", backgroundColor: "#CB2424", borderRadius: "4%", 
-      marginTop: "2%", marginLeft: "2%" }}>
+    <div style={{ width: "135px", height: "30px", backgroundColor: "#CB2424", borderRadius: "6%" }}>
       <span style={{ fontSize: "20px", marginLeft: "7px" }}>Not Validated</span>
     </div>
   )
--- frontend/src/components/MainNavWrapper/staging/pages/Validated.js
+++ frontend/src/components/MainNavWrapper/staging/pages/Validated.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Validated() {
   return (
-    <div style={{ width: "135", height: "30px", backgroundColor: "#4CC941", borderRadius: "4%", 
-      marginTop: "2%", marginLeft: "2%" }}>
+    <div style={{ width: "100px", height: "30px", backgroundColor: "#4CC941", borderRadius: "6%" }}>
       <span style={{ fontSize: "20px", marginLeft: "7px" }}>Validated</span>
     </div>
   )
