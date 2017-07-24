9aea874bcd343e62706f4751b07a99fe511eede8
Vi Gi <vigneshnataraj@gmail.com>
2017-07-24 15:18:56 +1000

    REACT: refactor styling

--- frontend/src/components/MainNavWrapper/staging/pages/StatusPage.js
+++ frontend/src/components/MainNavWrapper/staging/pages/StatusPage.js
@@ -10,7 +10,26 @@ import GoX from 'react-icons/lib/go/x'
 export default function StatusPage({
   status
 }){
-  
+  const quoteField = { 
+    flexDirection: "row",
+    width: "75%" 
+  }
+
+  const quoteTrue = {
+    flexDirection: "row",
+    width: "10%",
+    margin: "auto 0 9px",
+    color: "#4CC941"
+  }
+
+  const quoteFalse = {
+    flexDirection: "row",
+    width: "10%",
+    margin: "auto 0 9px",
+    color: "#CB2424"
+  }
+
+
   return (
     <div className="statusPage" style={{ marginLeft: "2%", marginTop: "2%" }}>
       
@@ -21,42 +40,42 @@ export default function StatusPage({
       {
         status.quoteSent ?
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Quote Sent</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#4CC941" }}><GoCheck size={20}/></span>
+          <h3 style={ quoteField }>Quote Sent</h3><span style={ quoteTrue }><GoCheck size={20}/></span>
         </div>
           :
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Quote Sent</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#CB2424" }}><GoX size={20} /></span>
+          <h3 style={ quoteField }>Quote Sent</h3><span style={ quoteFalse }><GoX size={20} /></span>
         </div>
         
       }
       {
         status.quoteAccepted ?
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Quote Accepted</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#4CC941" }}><GoCheck size={20}/></span>
+          <h3 style={ quoteField }>Quote Accepted</h3><span style={ quoteTrue }><GoCheck size={20}/></span>
         </div>
           :
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Quote Accepted</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#CB2424" }}><GoX size={20} /></span>
+          <h3 style={ quoteField }>Quote Accepted</h3><span style={ quoteFalse }><GoX size={20} /></span>
         </div>
       }
       {
         status.idVerified ?
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Id Verified</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#4CC941" }}><GoCheck size={20}/></span>
+          <h3 style={ quoteField }>Id Verified</h3><span style={ quoteTrue }><GoCheck size={20}/></span>
         </div>
         :
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Id Verified</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#CB2424" }}><GoX size={20} /></span>
+          <h3 style={ quoteField }>Id Verified</h3><span style={ quoteFalse }><GoX size={20} /></span>
         </div>
       }
       {
         status.depositCleared ?
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Deposit Cleared</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#4CC941" }}><GoCheck size={20}/></span>
+          <h3 style={ quoteField }>Deposit Cleared</h3><span style={ quoteTrue }><GoCheck size={20}/></span>
         </div>
         :
         <div style={{ display: "flex", width: "25%" }}>
-          <h3 style={{ flexDirection: "row", width: "75%" }}>Deposit Cleared</h3><span style={{ flexDirection: "row", width: "10%", margin: "auto 0 9px", color: "#CB2424" }}><GoX size={20} /></span>
+          <h3 style={ quoteField }>Deposit Cleared</h3><span style={ quoteFalse }><GoX size={20} /></span>
         </div>
       }
     </div>
